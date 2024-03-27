397d3d57ef97e5edc8a1ff735265e81a2bad7f41
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-03-27 16:11:01 -0300

    2 images

--- TRABALHO 1/1_canvasGlut/src/main.cpp
+++ TRABALHO 1/1_canvasGlut/src/main.cpp
@@ -21,15 +21,10 @@ int screenWidth = 500, screenHeight = 500;
 int opcao  = 50; //variavel global para selecao do que sera exibido na canvas.
 int mouseX, mouseY; //variaveis globais do mouse para poder exibir dentro da render().
 
-Bmp *bmp = NULL;
-unsigned char *data = NULL;
-GLuint textureID[MAX_TEXTURES];
+Bmp *image_1 = NULL;
+Bmp *image_2 = NULL;
 
-float ang=0;
-
-
-void DrawMouseScreenCoords()
-{
+void DrawMouseScreenCoords(){
     char str[100];
     sprintf(str, "Mouse: (%d,%d)", mouseX, mouseY);
     CV::text(10,300, str);
@@ -37,20 +32,32 @@ void DrawMouseScreenCoords()
     CV::text(10,320, str);
 }
 
+void DrawImage(Bmp *image){
+   for(int i = 0; i < image_1->getHeight(); i++)
+   {
+      for(int j = 0; j < image_1->getWidth(); j++)
+      {
+         
+      }
+   }
+}
+
 
 void render()
 {
-   CV::text(20,500,"Programa Demo Canvas2D");
-
    CV::translate(250, 250);
 
-   for(int i = 0; i < bmp->getHeight(); i++)
+   for(int i = 0; i < image_1->getHeight(); i++)
    {
-      for(int j = 0; j < bmp->getWidth(); j++)
+      for(int j = 0; j < image_1->getWidth(); j++)
       {
-         int pos = i*bmp->getWidth()*3 + j*3;
-         CV::color(bmp->getImage()[pos]/255.0, bmp->getImage()[pos+1]/255.0, bmp->getImage()[pos+2]/255.0);
+         int pos = i*image_1->getWidth()*3 + j*3;
+         int pos_2 = i*image_2->getWidth()*3 + j*3;
+         CV::color(image_1->getImage()[pos]/255.0, image_1->getImage()[pos+1]/255.0, image_1->getImage()[pos+2]/255.0);
          CV::rectFill(j, i, j + 1, i + 1);
+
+         CV::color(image_2->getImage()[pos_2]/255.0, image_2->getImage()[pos_2+1]/255.0, image_2->getImage()[pos_2+2]/255.0);
+         CV::rectFill(j + 200, i, j + 201, i + 1);
       }
    }
 
@@ -86,7 +93,7 @@ void mouse(int button, int state, int wheel, int direction, int x, int y)
    mouseX = x; //guarda as coordenadas do mouse para exibir dentro da render()
    mouseY = y;
 
-   printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
+   // printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
 }
 
 
@@ -94,9 +101,11 @@ int main()
 {
    CV::init(screenHeight, screenWidth, "Canvas2D");
 
-   bmp = new Bmp(".\\images\\pinguim.bmp");
-   bmp->convertBGRtoRGB();
+   image_1 = new Bmp(".\\images\\pinguim.bmp");
+   image_1->convertBGRtoRGB();
 
+   image_2 = new Bmp(".\\images\\bmp_24.bmp");
+   image_2->convertBGRtoRGB();
 
    CV::run();
    
